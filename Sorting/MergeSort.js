182874be04b32f6bab9c4f421bc6a991f04c1b4d
mahed <Darshana.Maheshwari@in.pega.com>
2023-10-28 23:43:13 +0530

    Added ds algo files

---
+++ Sorting/MergeSort.js
@@ -0,0 +1,40 @@
+// divide and conquer algo
+
+//best av and worts Space = O(n) Time = O(n(logn))
+
+function mergeSort(array) {
+    if(array.length <=1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array , 0, array.length -1 , auxiliaryArray);
+    return array;
+  }
+  
+  function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray){
+    if(startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray);
+    mergeSortHelper(auxiliaryArray, middleIdx +1, endIdx, mainArray);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray);
+  }
+  
+  function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray){
+    let k= startIdx;
+    let i = startIdx;
+    let j= middleIdx +1;
+    while(i <= middleIdx && j<= endIdx){
+      if(auxiliaryArray[i] <= auxiliaryArray[j]){
+        mainArray[k++] = auxiliaryArray[i++];
+      }else{
+        mainArray[k++] = auxiliaryArray[j++];
+      }
+    }
+    while(i<= middleIdx){
+      mainArray[k++] = auxiliaryArray[i++];
+    }
+    while(j <= endIdx){
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+  // Do not edit the line below.
+  exports.mergeSort = mergeSort;
+  
